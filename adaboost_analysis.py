e9d58606da795c1378713514e5a8370c13403bd3
Mads Jensen <mads@cnru.dk>
2016-11-23 11:19:13 +0100

    Cross-validation of grid search

--- adaboost_analysis.py
+++ adaboost_analysis.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import AdaBoostRegressor
-from sklearn.cross_validation import (StratifiedKFold, LeaveOneOut,
+from sklearn.cross_validation import (ShuffleSplit, LeaveOneOut,
                                       cross_val_score)
 from sklearn.grid_search import GridSearchCV
 # from sklearn.pipeline import make_pipeline
 import os
-import xgboost as xgb
 
 data_path = "/Users/au194693/projects/hyp_struct/data"
 # data_path = "/projects/MINDLAB2011_33-MR-high-order-cogn/" +\
@@ -32,37 +31,33 @@ y = np.asarray(y)
 X = X[2:, :-1]
 y = y[2:]
 
-cv = StratifiedKFold(y, n_folds=7, shuffle=True)
+cv = ShuffleSplit(len(y), test_size=0.2)
 loo = LeaveOneOut(len(y))
 
-ada = AdaBoostRegressor()
+grid_estimators = []
+scores_list = []
 
-adaboost_params = {
-    "n_estimators": np.arange(2, 80, 1),
-    "learning_rate": np.arange(0.1, 1.1, 0.1)
-}
+for train_cv, test_cv in cv:
+    # Setup grid parameters
+    ada = AdaBoostRegressor()
+    adaboost_params = {
+        "n_estimators": np.arange(2, 80, 1),
+        "learning_rate": np.arange(0.1, 1.1, 0.1)
+    }
 
-grid = GridSearchCV(
-    ada, param_grid=adaboost_params, cv=loo, verbose=1, n_jobs=2)
-grid.fit(X, y)
+    # Make grid search
+    grid = GridSearchCV(
+        ada,
+        param_grid=adaboost_params,
+        scoring="mean_squared_error",
+        verbose=1,
+        n_jobs=1)
+    grid.fit(X[train_cv], y[train_cv])
 
-ada_cv = grid.best_estimator_
+    # Select winning params
+    ada_cv = grid.best_estimator_
+    grid_estimators.append(ada_cv)
 
-scores = cross_val_score(ada_cv, X, y, cv=loo, scoring="mean_squared_error")
-
-# XGboost
-cv_params = {
-    "learning_rate": np.arange(0.1, 1.1, 0.1),
-    "max_depth": [1, 2, 3, 4, 5, 6, 7],
-    "n_estimators": np.arange(10, 86, 2)
-}
-
-grid = GridSearchCV(
-    xgb.XGBClassifier(),
-    cv_params,
-    # scoring='accuracy',
-    cv=cv,
-    n_jobs=2,
-    verbose=1)
-grid.fit(X, y_group)
-xgb_cv = grid.best_estimator_
+    scores_list.append(
+        cross_val_score(
+            ada_cv, X[test_cv], y[test_cv], scoring="neg_mean_squared_error"))
